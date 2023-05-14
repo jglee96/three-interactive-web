11029f0a0f1852ca846de0db3ff013fdc3db1a80
jglee96 <zakklee96@gmail.com>
2023-05-14 21:16:11 +0900

    add: 조명 추가하기
    * MeshBasicMaterial은 light의 영향을 받지 않음
    * DirectionalLight은 직사광선 -> 그림자 생성
    * AmbientLight은 밝기 -> 그림자 X

--- 1-cube/src/main.ts
+++ 1-cube/src/main.ts
@@ -24,16 +24,22 @@ function init() {
   );
 
   const geometry = new THREE.BoxGeometry(1, 1, 1);
-  const material = new THREE.MeshBasicMaterial({ color: 0xcc99ff });
+  const material = new THREE.MeshStandardMaterial({ color: 0xcc99ff });
 
   const cube = new THREE.Mesh(geometry, material);
-
   scene.add(cube);
 
   // camera.position.z = 5;
   camera.position.set(3, 4, 5);
-
   camera.lookAt(cube.position);
+
+  const directionalLight = new THREE.DirectionalLight(0xf0f0f0, 1);
+  directionalLight.position.set(-1, 2, 3);
+  scene.add(directionalLight);
+
+  const ambientLight = new THREE.AmbientLight(0xfffff, 0.1);
+  ambientLight.position.set(3, 2, 1);
+  scene.add(ambientLight);
 
   renderer.render(scene, camera);
 }
