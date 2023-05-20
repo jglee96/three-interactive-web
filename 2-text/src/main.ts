f4fe11cc86fd048304a7cf82967d2835636327c5
jglee96 <zakklee96@gmail.com>
2023-05-20 23:17:33 +0900

    add: 텍스트 추가하기
    * text geometry 생성해서 font, text 전달하면 됨

--- 2-text/src/main.ts
+++ 2-text/src/main.ts
@@ -1,12 +1,16 @@
 import * as THREE from "three";
 import { FontLoader } from "three/examples/jsm/loaders/FontLoader.js";
-import typeface from "../assets/fonts/The Jamsil 3 Regular_Regular.json"
+import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry.js";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import GUI from "lil-gui";
 
 window.addEventListener("load", function () {
   init();
 });
 
 function init() {
+  const gui = new GUI();
+
   const renderer = new THREE.WebGLRenderer({
     antialias: true,
   });
@@ -27,23 +31,45 @@ function init() {
   camera.position.z = 5;
 
   /**
+   * control
+   */
+  new OrbitControls(camera, renderer.domElement);
+
+  /**
    * font
    */
   const fontLoader = new FontLoader();
 
-  // fontLoader.load(
-  //   "./assets/fonts/The Jamsil 3 Regular_Regular.json",
-  //   (font) => {
-  //     console.log("load", font);
-  //   },
-  //   (event) => {
-  //     console.log("progress", event);
-  //   },
-  //   (error) => {
-  //     console.log("error", error);
-  //   }
-  // );
-  const font = fontLoader.parse(typeface);
+  fontLoader.load(
+    "./assets/fonts/The Jamsil 3 Regular_Regular.json",
+    (font) => {
+      /**
+       * test
+       */
+      const textGeometry = new TextGeometry("안녕, 친구들.", {
+        font,
+        size: 0.5,
+        height: 0.1,
+      });
+      const textMaterial = new THREE.MeshPhongMaterial({ color: 0x00c896 });
+
+      const text = new THREE.Mesh(textGeometry, textMaterial);
+      scene.add(text);
+    }
+  );
+
+  /**
+   * light
+   */
+  const ambientLight = new THREE.AmbientLight(0xffffff, 1);
+  scene.add(ambientLight);
+
+  const pointLight = new THREE.PointLight(0xffffff, 0.5);
+  const pointLightHelper = new THREE.PointLightHelper(pointLight, 0.5);
+  pointLight.position.set(3, 0, 2);
+  scene.add(pointLight, pointLightHelper);
+
+  gui.add(pointLight.position, "x").min(-3).max(3).step(0.1);
 
   render();
 
