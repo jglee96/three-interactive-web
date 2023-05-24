eeda8defc1e8325791fab0755fa6bad8b8a3ffb6
jglee96 <zakklee96@gmail.com>
2023-05-25 00:01:25 +0900

    add: 파티클 색상 추가

--- 5-fireworks/src/FireWork.ts
+++ 5-fireworks/src/FireWork.ts
@@ -31,6 +31,7 @@ class FireWork {
       alphaMap: texture,
       transparent: true,
       depthWrite: false,
+      color: new THREE.Color(Math.random(), Math.random(), Math.random()),
     });
 
     const points = new THREE.Points(particlesGeometry, particlesMaterial);
