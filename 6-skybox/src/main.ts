0af78b1f637ac818acd63b6dd67515bae112485b
jglee96 <zakklee96@gmail.com>
2023-05-25 23:43:19 +0900

    add: 큐브맵 텍스처를 이용하여 3차원 공간 표현
    * box를 크게 만들어서 3차원 공간 표현

---
+++ 6-skybox/src/main.ts
@@ -0,0 +1,76 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+
+window.addEventListener("load", function () {
+  init();
+});
+
+function init() {
+  const renderer = new THREE.WebGLRenderer({
+    antialias: true,
+  });
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  document.body.appendChild(renderer.domElement);
+
+  const scene = new THREE.Scene();
+
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    1,
+    10000
+  );
+
+  camera.position.z = 5;
+
+  const controls = new OrbitControls(camera, renderer.domElement);
+
+  controls.minDistance = 5;
+  controls.maxDistance = 100;
+
+  const textureLoader = new THREE.TextureLoader().setPath(
+    "assets/textures/Yokohama/"
+  );
+
+  const images = [
+    "posx.jpg",
+    "negx.jpg",
+    "posy.jpg",
+    "negy.jpg",
+    "posz.jpg",
+    "negz.jpg",
+  ];
+
+  const geometry = new THREE.BoxGeometry(5000, 5000, 5000);
+  const materials = images.map(
+    (image) =>
+      new THREE.MeshBasicMaterial({
+        map: textureLoader.load(image),
+        side: THREE.BackSide,
+      })
+  );
+
+  const skybox = new THREE.Mesh(geometry, materials);
+
+  scene.add(skybox);
+
+  render();
+
+  function render() {
+    renderer.render(scene, camera);
+
+    requestAnimationFrame(render);
+  }
+
+  function handleResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.render(scene, camera);
+  }
+
+  window.addEventListener("resize", handleResize);
+}
